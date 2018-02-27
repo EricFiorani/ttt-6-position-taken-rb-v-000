e94a8c0242253c43b8589230a8ae1da9a6876385
Eric Fiorani <efiorani305@gmail.com>
2018-02-27 16:34:01 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ def position_taken?(board, index)
   if board[index] == "  " || " "
     return false
   end
-else board[index] == "X" || "0"
-  return true
-end
+  else board[index] == "X" || "0"
+    return true
+  end
 end
